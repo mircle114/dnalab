216f1e7cb2562c8de216bb4fd72dcd39ca31175d
Marcel Leger <32421611+mircle114@users.noreply.github.com>
2022-02-15 22:07:20 +0000

    Refactoring ColorPicky - GridBagLayout examples

--- src/main/java/ColorPicky.java
+++ src/main/java/ColorPicky.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.MouseAdapter;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -13,12 +15,14 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+
 
 public class ColorPicky {
 
@@ -46,52 +50,118 @@ public class ColorPicky {
         });
     }
 
-    public class TestPane extends JPanel {
+    public class TestPane extends JPanel implements ActionListener  
+    {
 
+        private JButton selFileButton;
+        
         private BufferedImage img;
-        private JLabel label;
-
+        private JLabel labelImg;
+     
         private JPanel fields;
         private JTextField red;
         private JTextField green;
         private JTextField blue;
 
-        public TestPane() {
+        private String fileName;
+
+        public String getFileName()
+        {
+          return this.fileName;
+        }
+
+        public void setFileName(String filename)
+        {
+          this.fileName = filename;
+        }
+
+        public TestPane() 
+        {
             setLayout(new GridBagLayout());
+            
             GridBagConstraints gbc = new GridBagConstraints();
-            gbc.gridwidth = GridBagConstraints.REMAINDER;
-            label = new JLabel();
-            try {
-                img = ImageIO.read(new File("src/main/resources/issue459.jpg"));
-                label.setIcon(new ImageIcon(img));
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            //gbc.gridwidth = GridBagConstraints.REMAINDER;
+            
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            
+            // Choose file button
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            selFileButton = new JButton("...");
+            buttonProperties(gbc);
 
-            add(label, gbc);
-
+            // The selected image file (using a label with setIcon)
+            labelImg = new JLabel();
+            gbc.gridx = 1;
+            gbc.gridy = 0;
+            add(labelImg,gbc);
+            
             fields = new JPanel();
-            fields.setBorder(new EmptyBorder(5, 5, 5, 5));
-            red = new JTextField(3);
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            fields.setBorder(new EmptyBorder(1, 15, 15, 15));
+            red = new JTextField(4);
             green = new JTextField(3);
             blue = new JTextField(3);
             fields.add(red);
             fields.add(green);
             fields.add(blue);
             add(fields, gbc);
+      }
 
-            label.addMouseMotionListener(new MouseAdapter() {
-                @Override
-                public void mouseMoved(MouseEvent e) {
+    public void loadImageIcon(String filename)
+    {
+        try 
+        {
+            if(fileName != "")
+            {
+              BufferedImage img = ImageIO.read(new File(fileName));
+              labelImg.setIcon(new ImageIcon(img));
+              labelImg.addMouseMotionListener(new MouseAdapter() 
+              {
+              @Override
+              public void mouseMoved(MouseEvent e) 
+              {
+                  try
+                  {
                     int packedInt = img.getRGB(e.getX(), e.getY());
                     Color color = new Color(packedInt, true);
                     fields.setBackground(color);
                     red.setText(Integer.toString(color.getRed()));
                     green.setText(Integer.toString(color.getGreen()));
                     blue.setText(Integer.toString(color.getBlue()));
-                }
-            });
+                  }
+                  catch(Exception exception)
+                  {
 
+                  }
+                }
+              });
+            }
+        } 
+        catch (IOException ex) 
+        {
+            ex.printStackTrace();
+        }
+    }
+
+        public void buttonProperties(GridBagConstraints gbc)
+        {
+            //selFileButton.setBounds(0,0,100,100);
+            add(selFileButton,gbc);
+            selFileButton.addActionListener(this);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) 
+        {
+            if(selFileButton instanceof JButton)
+            {
+                DnaFileChooser dnFile = new DnaFileChooser(this);
+                String selectedFileName = dnFile.getSelectedFile().getAbsolutePath();
+                this.setFileName(selectedFileName);
+                if(selectedFileName != "") {loadImageIcon(selectedFileName);}
+            }
         }
 
     }
---
+++ src/main/java/DnaFileChooser.java
@@ -0,0 +1,26 @@
+package com.dnavault;
+
+import javax.swing.*;
+import javax.swing.JFileChooser;
+import java.io.File;
+
+public class DnaFileChooser extends JFileChooser
+{
+  public DnaFileChooser(JPanel displayOn)
+  {
+      this.setCurrentDirectory(new File(System.getProperty("user.home")));
+      int result = this.showOpenDialog(displayOn);
+      if (result == JFileChooser.APPROVE_OPTION) {
+        File selectedFile = this.getSelectedFile();
+        System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+      }
+  }
+
+  public static void main(String[] args)
+  {
+      
+  }
+
+
+
+}
